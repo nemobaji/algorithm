961db7e74523b15517210522d229b0c9ebb63611
김현우 <155235090+nemobaji@users.noreply.github.com>
2025-10-16 21:29:23 +0900

    Create merge-intervals.py

---
+++ sorting/merge-intervals.py
@@ -0,0 +1,11 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        merged = []
+      
+        for i in sorted(intervals, key=lambda x: x[0]):
+            if merged and (merged[-1][1] >= i[0]):
+                merged[-1][1] = max(merged[-1][1], i[1])
+            else: 
+                merged.append(i)
+              
+        return merged
