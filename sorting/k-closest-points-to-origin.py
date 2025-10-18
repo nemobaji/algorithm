79bfc6a96d3a54774d1897ac2a84a94b97e649c8
김현우 <155235090+nemobaji@users.noreply.github.com>
2025-10-18 15:38:09 +0900

    Update k-closest-points-to-origin.py
    Commented out the Solution class and kClosest method.

--- sorting/k-closest-points-to-origin.py
+++ sorting/k-closest-points-to-origin.py
@@ -12,3 +12,8 @@ class Solution:
             result.append((x, y))
         
         return result
+
+# class Solution:
+#     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+#         points.sort(key=lambda x: (x[0]**2 + x[1]**2))
+#         return points[:k]
